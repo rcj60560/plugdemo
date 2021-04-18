96ef1bce534c9c0ec81b8aa18bdb84ed8d995763
rcj60560 <63560560>
2021-04-18 20:30:29 +0800

    配置组件化参数

--- app/src/main/java/com/luocj/plugdemo/MainActivity.java
+++ app/src/main/java/com/luocj/plugdemo/MainActivity.java
@@ -2,7 +2,12 @@ package com.luocj.plugdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.luocj.user.UserMainActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void go2User(View view) {
+        startActivity(new Intent(this, UserMainActivity.class));
     }
 }
