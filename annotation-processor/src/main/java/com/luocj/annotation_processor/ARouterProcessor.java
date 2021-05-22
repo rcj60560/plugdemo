c3611261cccda8205c9a0ec6ccf886f56ef62266
rcj60560 <289759379@qq.com>
2021-05-22 17:43:25 +0800

    javapoet 生成代码

--- annotation-processor/src/main/java/com/luocj/annotation_processor/ARouterProcessor.java
+++ annotation-processor/src/main/java/com/luocj/annotation_processor/ARouterProcessor.java
@@ -1,17 +1,34 @@
 package com.luocj.annotation_processor;
 
+import com.google.auto.service.AutoService;
+import com.luocj.annotation.ARouter;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
+
+import java.io.IOException;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.Filer;
 import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
 import javax.tools.Diagnostic;
 
+@AutoService(Processor.class) // 编译期绑定
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+@SupportedAnnotationTypes({"com.luocj.annotation.ARouter"}) // 表示我要处理那个注解
 public class ARouterProcessor extends AbstractProcessor {
 
     private Elements elementUtils;
@@ -31,7 +48,47 @@ public class ARouterProcessor extends AbstractProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        messager.printMessage(Diagnostic.Kind.NOTE,"------_>>>>>>>>>>>>");
+
+        if (annotations.isEmpty()) {
+            messager.printMessage(Diagnostic.Kind.NOTE, "----->>>>>>>>>>>>> " + ", 没有被ARouter注解的类");
+            return false;
+        }
+
+        Set<? extends Element> elements = roundEnv.getElementsAnnotatedWith(ARouter.class);
+        for (Element element : elements) {
+            PackageElement packageName = elementUtils.getPackageOf(element);
+            messager.printMessage(Diagnostic.Kind.NOTE, "paakageName:" + packageName.toString());
+
+            String className = element.getSimpleName().toString();
+            messager.printMessage(Diagnostic.Kind.NOTE, "className:" + className);
+
+        }
+
+        generateHelloWorld();
+
         return false;
+    }
+
+    private void generateHelloWorld() {
+        MethodSpec main = MethodSpec.methodBuilder("main")
+                .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                .returns(void.class)
+                .addParameter(String[].class, "args")
+                .addStatement("$T.out.println($S)", System.class, "Hello, JavaPoet222222222222!")
+                .build();
+
+        TypeSpec helloWorld = TypeSpec.classBuilder("HelloWorld")
+                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                .addMethod(main)
+                .build();
+
+        JavaFile javaFile = JavaFile.builder("com.example.helloworld", helloWorld)
+                .build();
+
+        try {
+            javaFile.writeTo(filer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
